6c3c2adb7f9833ce6f7ff70b7f11ef95db5ca173
JimJokes <543001037@qq.com>
2017-05-19 15:30:55 +0800

    rewrite keepalive

--- chat/room.py
+++ chat/room.py
@@ -1,5 +1,4 @@
 import time
-import logging
 import threading
 
 from chat.network.client import Client
@@ -8,20 +7,27 @@ RAW_BUFF_SIZE = 4096
 KEEP_ALIVE_INTERVAL_SECONDS = 45
 
 
-def keep_alive(client, delay):
-    while True:
-        print('发送心跳验证')
-        currents_ts = int(time.time())
-        client.send({
-            'type': 'keeplive',
-            'tick': currents_ts
-        })
-        time.sleep(delay)
+class KeepAlive(threading.Thread):
+    def __init__(self, client, delay):
+        super(KeepAlive, self).__init__()
+        self.client = client
+        self.delay = delay
+        self.stop = False
+
+    def run(self):
+        while True:
+            if self.stop:
+                raise SystemExit
+            print('发送心跳验证')
+            currents_ts = int(time.time())
+            self.client.send({
+                'type': 'keeplive',
+                'tick': currents_ts
+            })
+            time.sleep(self.delay)
 
 
 class ChatRoom:
-    client = None
-    room_id = None
     channel_id = -9999
 
     callbacks = {}
@@ -29,42 +35,46 @@ class ChatRoom:
     def __init__(self, room_id):
         self.room_id = room_id
         self.client = Client()
+        # self.stop = False
 
-    def on(self, event_name, callback):
-        callback_list = None
-        try:
-            callback_list = self.callbacks[event_name]
-        except KeyError:
-            callback_list = []
-            self.callbacks[event_name] = callback_list
-        callback_list.append(callback)
-        # print(self.callbacks)
-
-    def trigger_callbacks(self, event_name, message):
-        callback_list = None
-        try:
-            callback_list = self.callbacks[event_name]
-        except KeyError:
-            logging.info('Message of type "%s" is not handled' % event_name)
-            return
-        # print(callback_list)
-        if callback_list is None or len(callback_list) <= 0:
-            return
-
-        for callback in callback_list:
-            callback(message)
+    # def on(self, event_name, callback):
+    #     callback_list = None
+    #     try:
+    #         callback_list = self.callbacks[event_name]
+    #     except KeyError:
+    #         callback_list = []
+    #         self.callbacks[event_name] = callback_list
+    #     callback_list.append(callback)
+    #     # print(self.callbacks)
+    #
+    # def trigger_callbacks(self, event_name, message):
+    #     callback_list = None
+    #     try:
+    #         callback_list = self.callbacks[event_name]
+    #     except KeyError:
+    #         logging.info('Message of type "%s" is not handled' % event_name)
+    #         return
+    #     # print(callback_list)
+    #     if callback_list is None or len(callback_list) <= 0:
+    #         return
+    #
+    #     for callback in callback_list:
+    #         callback(message)
 
     def knock(self):
         try:
-            print('发送登录申请')
+            # print('发送登录申请')
             self.client.send({'type': 'loginreq', 'roomid': self.room_id})
         except Exception as e:
             print(e)
-        app = threading.Thread(target=keep_alive, args=(self.client, KEEP_ALIVE_INTERVAL_SECONDS))
-        app.setDaemon(True)
-        app.start()
+        # app = KeepAlive(self.client, KEEP_ALIVE_INTERVAL_SECONDS)
+        # app.setDaemon(True)
+        # app.start()
 
         for message in self.client.receive():
+            # if self.stop:
+            #     app.stop = True
+
             if not message:
                 continue
 
@@ -83,4 +93,3 @@ class ChatRoom:
             yield message
     # todo: 断开连接
     # def cutoff(self):
-
