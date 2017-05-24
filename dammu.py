feb86db45b8ff4397d9eeefd482cba0db92238f1
JimJokes <543001037@qq.com>
2017-05-24 15:39:52 +0800

    add reconnect

--- dammu.py
+++ dammu.py
@@ -1,6 +1,7 @@
 import threading
 import time
 from tkinter import END
+from tkinter.messagebox import askyesnocancel
 
 from utils import align_right
 from room import ChatRoom, KeepAlive, KEEP_ALIVE_INTERVAL_SECONDS
@@ -10,13 +11,15 @@ i = 0
 
 
 class Danmu(threading.Thread):
-    def __init__(self, text, text_star, roomid, stars):
+    def __init__(self, text, text_star, roomid, stars, on, off):
         super(Danmu, self).__init__()
         self.text = text
         self.text_star = text_star
         self._roomid = roomid
         self.stars = stars
         self.stop = False
+        self.on = on
+        self.off = off
 
     def run(self):
         _room = ChatRoom(self._roomid)
@@ -25,39 +28,47 @@ class Danmu(threading.Thread):
         app.start()
 
         self.text.insert(END, '开始监控[%s]的直播间弹幕！\n' % self._roomid)
-        for msg in _room.knock():
-            if self.stop:
-                # _room.stop = True
-                app.stop = True
-                raise SystemExit
-            try:
-                msg_type = msg.attr('type')
-                now = time.strftime("%y-%m-%d %H:%M:%S", time.localtime())
-                _roomid = msg.attr('rid')
+        try:
+            for msg in _room.knock():
+                if self.stop:
+                    # _room.stop = True
+                    app.stop = True
+                    raise SystemExit
+                try:
+                    msg_type = msg.attr('type')
+                    now = time.strftime("%y-%m-%d %H:%M:%S", time.localtime())
+                    _roomid = msg.attr('rid')
 
-                if msg_type == 'chatmsg':
-                    _ct = msg.attr('ct')
-                    if _ct is None:
-                        _ct = '网页'
-                    else:
-                        _ct = '手机'
-                    _uname = msg.attr('nn')
-                    message = align_right('[%s] %s' % (_ct, _uname), 25) + ':%s' % msg.attr('txt')
-                    if _uname in self.stars:
-                        self.update_star(message)
-                    self.update_danmu(message)
+                    if msg_type == 'chatmsg':
+                        # _ct = msg.attr('ct')
+                        # if _ct is None:
+                        #     _ct = '网页'
+                        # else:
+                        #     _ct = '手机'
+                        _uname = msg.attr('nn')
+                        _lv = msg.attr('level')
+                        message = align_right('[LV:%s] ' % _lv, 9) + align_right('%s' % _uname, 21) + ':%s' % msg.attr('txt')
+                        if _uname in self.stars:
+                            self.update_star(message)
+                        self.update_danmu(message)
 
-                if msg_type == 'uenter':
-                    _uname = msg.attr('nn')
-                    if _uname in self.stars:
-                        message = '%s 进入了直播间！' % _uname
-                        self.update_star(message)
-                # elif msg_type == 'uenter':
-                #     message = '%s [%s]:[%s][等级:%s] 进入了直播间!' % (now, rooms[_roomid], _uname, msg.attr('level'))
-                #     yield message
+                    if msg_type == 'uenter':
+                        _uname = msg.attr('nn')
+                        if _uname in self.stars:
+                            message = '%s 进入了直播间！' % _uname
+                            self.update_star(message)
+                    # elif msg_type == 'uenter':
+                    #     message = '%s [%s]:[%s][等级:%s] 进入了直播间!' % (now, rooms[_roomid], _uname, msg.attr('level'))
+                    #     yield message
 
-            except KeyError:
-                continue
+                except KeyError:
+                    continue
+        except ConnectionError:
+            self.off()
+            flag = askyesnocancel('弹幕服务器连接失败', '是否重新连接弹幕服务器？')
+            if flag:
+                self.on()
+            raise SystemExit
 
     def update_danmu(self, message):
         global j
