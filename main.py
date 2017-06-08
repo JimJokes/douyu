25596498b9c4270b1c81ba5ad0f0fbc0d310e441
JimJokes <543001037@qq.com>
2017-06-08 17:44:28 +0800

    adjust master window

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from logger import Logger
 
 if __name__ == '__main__':
     logger = Logger(name='main')
-    root = View()
-    root.master.title('斗鱼弹幕姬')
-    root.master.minsize(height=500, width=800)
+    root = View(1000, 600)
+    root.title('斗鱼弹幕姬')
+    root.minsize(height=500, width=800)
     root.mainloop()
