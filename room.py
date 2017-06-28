0957bc13372791de2eb65086dc5aa09adb2d3d2f
JimJokes <543001037@qq.com>
2017-06-28 18:05:01 +0800

    rewrite room

--- room.py
+++ room.py
@@ -7,6 +7,7 @@ try:
     from client_test import Client
 except ImportError:
     from client import Client
+from utils import ReplyMessage
 
 RAW_BUFF_SIZE = 4096
 KEEP_ALIVE_INTERVAL_SECONDS = 45
@@ -35,9 +36,56 @@ class KeepAlive(threading.Thread):
             time.sleep(self.delay)
 
 
-class ChatRoom:
+class ChatRoom(threading.Thread):
     channel_id = -9999
 
-    def __init__(self, room):
+    def __init__(self, room, result_q):
+        super(ChatRoom, self).__init__()
         self.room = room
+        self.result_q = result_q
         self.client = Client()
+        self.alive = threading.Event()
+        self.alive.set()
+
+    def run(self):
+        self.connect()
+        while self.alive:
+            self.receive()
+
+    def receive(self):
+        res = self.client.receive()
+
+        if res.type == ReplyMessage.ERROR:
+            if res.code == 401:
+                self.quit_group()
+            self.client.disconnect()
+            self.connect()
+
+        elif res.type == ReplyMessage.SUCCESS:
+            message = res.data
+
+    def connect(self):
+        while True:
+            res = self.client.connect()
+
+            if res.type == ReplyMessage.SUCCESS:
+                break
+            elif res.type == ReplyMessage.WARNING:
+                res.code = 2000
+                res.data = '弹幕服务器连接错误，请重新连接！'
+                self.result_q.put(res)
+
+            time.sleep(1)
+
+    def join_group(self):
+        data = {'type': 'joingroup', 'rid': self.room, 'gid': self.channel_id}
+        res = self.client.send_msg(data)
+
+        if res == ReplyMessage.SUCCESS:
+            res.code = 2000
+            res.data = '已连接到弹幕服务器，房间id：%s' % self.room
+            self.result_q.put(res)
+
+    def quit_group(self):
+        data = {'type': 'logout'}
+        self.client.send_msg(data)
