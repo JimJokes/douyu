96ddaf1707bb98e38f39cedec323decfa775f236
JimJokes <543001037@qq.com>
2017-06-29 18:05:20 +0800

    process message

--- room.py
+++ room.py
@@ -3,6 +3,7 @@ import time
 
 import logging
 
+import utils
 from message import Message
 from packet import Packet
 
@@ -42,6 +43,11 @@ class KeepAlive(threading.Thread):
 
 class ChatRoom(threading.Thread):
     channel_id = -9999
+    cq = {
+        '1': '初级酬勤',
+        '2': '中级酬勤',
+        '3': '高级酬勤',
+    }
 
     def __init__(self, room, result_q):
         super(ChatRoom, self).__init__()
@@ -54,19 +60,70 @@ class ChatRoom(threading.Thread):
     def run(self):
         self.connect()
         while self.alive:
-            self.receive()
+            message = self.receive()
+            if message is None:
+                continue
+            else:
+                self._handle_message(message)
+
+    def _handle_message(self, message):
+        data = {}
+        msg_type = message.attr('type')
+        data['time'] = self.now_time()
+        data['type'] = msg_type
+        if msg_type == 'chatmsg':
+            data['nn'] = message.attr('nn')
+            data['lv'] = message.attr('level')
+            data['txt'] = message.attr('txt')
+
+        elif msg_type == 'uenter':
+            data['nn'] = message.attr('nn')
+            data['txt'] = '进入了直播间！'
+
+        elif msg_type == 'dgb':
+            data['nn'] = message.attr('nn')
+            gfid = message.attr('gfid')
+            try:
+                data['gift'] = utils.gifts[gfid]
+            except KeyError:
+                data['gift'] = '未知礼物%s' % gfid
+            data['hit'] = message.attr('hits') or 1
+
+        elif msg_type == 'bc_buy_deserve':
+            data['nn'] = message.attr('sui')['nick']
+            data['cp'] = self.cq[message.attr('lev')]
+            data['hit'] = message.attr('hits')
+
+        elif msg_type == 'spbc':
+            data['nn'] = message.attr('sn')
+            data['gift'] = message.attr('gn')
+            data['dn'] = message.attr('dn')
+            data['room'] = message.attr('drid')
+
+        self.result_q.put(data)
+
+    def now_time(self):
+        return time.strftime('%m-%d %H:%M:%S', time.localtime())
 
     def receive(self):
         res = self.client.receive()
 
         if res.type == ReplyMessage.ERROR:
-            if res.code == 401:
+            if res.code == 401 or 402:
                 self.quit_group()
             self.client.disconnect()
             self.connect()
 
         elif res.type == ReplyMessage.SUCCESS:
-            message = res.data
+            data = res.data
+            try:
+                buff = data.decode()
+                message = Message.sniff(buff)
+                return message
+            except UnicodeDecodeError as e:
+                logger.info(e)
+                logger.info(data)
+                return None
 
     def connect(self):
         while True:
@@ -96,3 +153,6 @@ class ChatRoom(threading.Thread):
 
     def quit(self):
         self.alive.clear()
+
+    def _reply(self, code=None, data=None):
+        return ReplyMessage(ReplyMessage.SUCCESS, code, data)
