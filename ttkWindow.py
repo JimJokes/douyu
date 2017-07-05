e59f4c0ba2984a5b19b6c343a8faa51a9afcf671
JimJokes <543001037@qq.com>
2017-07-05 10:29:23 +0800

    inherit Window

--- ttkWindow.py
+++ ttkWindow.py
@@ -4,11 +4,15 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.font import Font
 from tkinter.messagebox import showwarning
+from queue import Queue
 
 import utils
 from popup_win import LivePopup, StarPopup
 
 star_file = os.path.join(os.getcwd(), 'starList.txt')
+if not os.path.exists(star_file):
+    with open(star_file, 'w', encoding='utf-8'):
+        pass
 
 if getattr(sys, 'frozen', False):
     icon = os.path.join(getattr(sys, '_MEIPASS', '.'), 'icon.ico')
@@ -36,7 +40,6 @@ class Window:
         self.font = Font(family='Microsoft YaHei', size=11)
         self.window()
         self.position()
-        self.read_stars()
 
     def position(self):
         screen_width = self.master.winfo_screenwidth()
@@ -71,9 +74,9 @@ class Window:
         notebook = ttk.Notebook(frame, padding=(10, 10, 10, 10))
 
         frame_danmaku = ttk.Frame()
-        danmaku_tree = self.tree_view(frame_danmaku, ('昵称', '弹幕'), x=1, y=1, style='tree.Treeview')
+        self.danmaku_tree = self.tree_view(frame_danmaku, ('昵称', '弹幕'), x=1, y=1, style='tree.Treeview')
         # danmaku_tree.column('等级', width=55, stretch=0)
-        danmaku_tree.column('昵称', width=180, stretch=0)
+        self.danmaku_tree.column('昵称', width=100, stretch=0)
 
         notebook.add(frame_danmaku, text='弹幕')
 
@@ -83,26 +86,28 @@ class Window:
 
         notebook.place(relheight=1, relwidth=1)
 
-        lock_button = ttk.Button(frame, textvariable=self.lock_text, width=5, command=self.lock)
+        self.lock_button = ttk.Button(frame, textvariable=self.lock_text, width=5)
         self.lock_text.set('锁屏')
-        lock_button.place(anchor=tk.NE, y=10, relx=0.9)
+        self.lock_button.place(anchor=tk.NE, y=10, relx=0.9)
 
     # 左侧关注窗口
     def window_star(self, frame):
         frame_top = ttk.Frame(frame)
         frame_top.place(relwidth=1, relheight=0.6, rely=0)
 
-        text_tree = self.tree_view(frame_top, ('时间', '昵称', '弹幕'), x=1, y=1, style='tree.Treeview')
-        text_tree.column('时间', stretch=0, width=160)
-        text_tree.column('昵称', stretch=0, width=180)
+        self.text_tree = self.tree_view(frame_top, ('时间', '昵称', '直播间', '弹幕'), x=1, y=1, style='tree.Treeview')
+        self.text_tree.column('时间', stretch=0, width=160)
+        self.text_tree.column('昵称', stretch=0, width=100)
+        self.text_tree.column('直播间', stretch=0, width=50)
 
         frame_bottom = ttk.Frame(frame)
         frame_bottom.place(relwidth=1, relheight=0.4, rely=0.6)
 
-        gift_tree = self.tree_view(frame_bottom, ('时间', '昵称', '礼物', '连击'), y=1, style='tree.Treeview')
-        gift_tree.column('时间', stretch=0, width=160)
-        gift_tree.column('昵称', stretch=0, width=180)
-        gift_tree.column('连击', stretch=0, width=50)
+        self.gift_tree = self.tree_view(frame_bottom, ('时间', '昵称', '直播间', '礼物', '连击'), y=1, style='tree.Treeview')
+        self.gift_tree.column('时间', stretch=0, width=160)
+        self.gift_tree.column('昵称', stretch=0, width=100)
+        self.gift_tree.column('直播间', stretch=0, width=50)
+        self.gift_tree.column('连击', stretch=0, width=50)
 
     # 右侧基础窗口
     def window_right(self, frame):
@@ -150,9 +155,9 @@ class Window:
         self.entry_id = ttk.Entry(frame)
         self.entry_id.place(relx=0.5, rely=0.1, height=20, relwidth=0.4)
 
-        self.start_button = ttk.Button(frame, text='连接', command=self.on)
+        self.start_button = ttk.Button(frame, text='连接')
         self.start_button.place(anchor=tk.NE, relx=0.4, rely=0.5, width=60)
-        self.stop_button = ttk.Button(frame, text='断开连接', state=tk.DISABLED, command=self.off)
+        self.stop_button = ttk.Button(frame, text='断开连接', state=tk.DISABLED)
         self.stop_button.place(relx=0.6, rely=0.5, width=60)
 
     # 关注列表设置窗口
@@ -177,7 +182,6 @@ class Window:
     # 表格视图创建
     def tree_view(self, frame, columns, x=None, y=None, style='Treeview'):
         tree = ttk.Treeview(frame, columns=columns, show='headings', style=style)
-        tree.columns = columns
         if x:
             xbar = ttk.Scrollbar(frame, orient=tk.HORIZONTAL, command=tree.xview)
             tree.config(xscrollcommand=xbar.set)
@@ -187,8 +191,8 @@ class Window:
             tree.config(yscrollcommand=ybar.set)
             ybar.pack(fill=tk.Y, side=tk.RIGHT)
 
-        for col in columns:
-            tree.heading(col, text=col)
+        for idx, col in enumerate(columns):
+            tree.heading(idx, text=col)
 
         tree.pack(fill=tk.BOTH, expand=1)
         return tree
@@ -196,56 +200,13 @@ class Window:
     # 表格插入并按文字长度调整格宽
     def insert(self, tree, values):
         for idx, value in enumerate(values):
-            value = value.replace('\n', '')
+            value = value.replace('\n', ' ') if isinstance(value, str) else value
+            values[idx] = value
             text_w = self.font.measure(value)
-            if tree.column(tree.columns[idx], width=None) < text_w:
-                tree.column(tree.columns[idx], width=text_w + 10)
-        tree.insert('', tk.END, values=values)
-
-    # 开始连接事件
-    def on(self):
-        room_id = self.entry_id.get()
-        if not room_id.isdigit():
-            showwarning('直播间ID不正确', '请输入正确的直播间ID！')
-        else:
-            self.start_button.config(state=tk.DISABLED)
-            self.stop_button.config(state=tk.ACTIVE)
-
-    # 断开连接事件
-    def off(self):
-        self.start_button.config(state=tk.ACTIVE)
-        self.stop_button.config(state=tk.DISABLED)
-
-    # 滚屏锁屏事件
-    def lock(self):
-        if utils.CheckVar:
-            utils.CheckVar = False
-            self.lock_text.set('滚屏')
-        else:
-            utils.CheckVar = True
-            self.lock_text.set('锁屏')
-
-    # 从文件读取关注列表
-    def read_stars(self):
-        self.stars.delete(1.0, tk.END)
-        if not os.path.exists(star_file):
-            with open(star_file, 'w', encoding='utf-8'):
-                pass
-        else:
-            with open(star_file, 'r', encoding='utf-8') as f:
-                lines = f.readlines()
-                for line in lines:
-                    utils.stars.append(line.strip())
-                    self.stars.insert(tk.END, line)
-
-    # 把关注列表存入文件
-    def save_stars(self):
-        text = self.stars.get(1.0, tk.END)
-        with open(star_file, 'w', encoding='utf-8') as f:
-            for star in text.split('\n'):
-                if star.strip():
-                    f.write(star.strip()+'\n')
-        self.read_stars()
+            if tree.column(idx, width=None) < text_w:
+                tree.column(idx, width=text_w + 10)
+        iid = tree.insert('', tk.END, values=values)
+        return iid
 
     # 开播提醒弹出窗口
     def live_popup(self, room=123, image='196_170622002601.jpg', title='【小缘】啵啵啵啵啵啵', status='直播中（已播112分钟）', name='123'):
@@ -299,9 +260,110 @@ class Window:
         self.popups.remove(win)
 
 
+class App(Window):
+    CheckVar = True
+    starList = []
+    i = 0
+    iid = None
+    room = None
+
+    def __init__(self, master):
+        super(App, self).__init__(master)
+        self.result_q = Queue()
+        self.gift_q = Queue()
+        self.read_stars()
+        self.event()
+
+    def event(self):
+        self.start_button.config(command=self.on)
+        self.stop_button.config(command=self.off)
+        self.lock_button.config(command=self.lock)
+        self.master.bind('<MESSAGE>', self._handle_message)
+
+    # 开始连接事件
+    def on(self):
+        room_id = self.entry_id.get()
+        if not room_id.isdigit():
+            showwarning('直播间ID不正确', '请输入正确的直播间ID！')
+        else:
+            self.start_button.config(state=tk.DISABLED)
+            self.stop_button.config(state=tk.ACTIVE)
+
+    # 断开连接事件
+    def off(self):
+        self.start_button.config(state=tk.ACTIVE)
+        self.stop_button.config(state=tk.DISABLED)
+
+    # 滚屏锁屏事件
+    def lock(self):
+        if self.CheckVar:
+            self.CheckVar = False
+            self.lock_text.set('滚屏')
+        else:
+            self.CheckVar = True
+            self.lock_text.set('锁屏')
+
+    # 从文件读取关注列表
+    def read_stars(self):
+        self.stars.delete(1.0, tk.END)
+        with open(star_file, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                self.starList.append(line.strip())
+                self.stars.insert(tk.END, line)
+
+    # 把关注列表存入文件
+    def save_stars(self):
+        text = self.stars.get(1.0, tk.END)
+        with open(star_file, 'w', encoding='utf-8') as f:
+            for star in text.split('\n'):
+                if star.strip():
+                    f.write(star.strip()+'\n')
+        self.read_stars()
+
+    # 消息处理
+    def _handle_message(self):
+        message = self.result_q.get()
+        msg_type = message.msg_type
+        if msg_type in ('error', 'loginres'):
+            self._update_danmaku(['', message.txt])
+        elif msg_type == 'chatmsg':
+            self._update_danmaku([message.nn, message.txt])
+            if message.nn in self.starList:
+                self._update_star_danmaku([message.time, message.nn, message.txt])
+        # elif msg_type == ''
+
+    # 更新全部弹幕
+    def _update_danmaku(self, values):
+        iid = self.insert(self.danmaku_tree, values)
+        if self.CheckVar:
+            self.danmaku_tree.see(iid)
+
+        if self.i == 0:
+            self.iid = iid
+        elif self.i > 3000:
+            delete_iid = self.iid
+            self.iid = self.danmaku_tree.next(delete_iid)
+            self.danmaku_tree.delete(delete_iid)
+        else:
+            self.i += 1
+
+    # 更新关注人弹幕
+    def _update_star_danmaku(self, values):
+        iid = self.insert(self.text_tree, values)
+        if self.CheckVar:
+            self.text_tree.see(iid)
+
+    # 更新关注人礼物
+    def _update_gift(self, values):
+        idd = self.insert(self.gift_tree, values)
+        if self.CheckVar:
+            self.gift_tree.see(idd)
+
+
 if __name__ == '__main__':
     root = tk.Tk()
-    win = Window(root)
+    Window(root)
     root.mainloop()
     # top = tk.Tk()
     # app = LivePopup(top)
