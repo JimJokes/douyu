594fedb235c8b0bf2a29fe448066f4154c7421ab
JimJokes <543001037@qq.com>
2017-07-07 17:45:35 +0800

    rewrite RoomInfo with async

--- roomInfo.py
+++ roomInfo.py
@@ -1,115 +1,116 @@
-import json
+import os
 import time
+import asyncio
+from tkinter import Tk
+
+import aiohttp
 import threading
-import urllib.request
-from http.client import IncompleteRead
-from urllib.error import URLError
+import requests
 from html import unescape
 
-import utils
 import logging
 logger = logging.getLogger('main.'+__name__)
 
-gift_api = 'https://capi.douyucdn.cn/api/v1/station_effect'
-room_url = 'https://www.douyu.com/196'
+GIFT_API = 'https://capi.douyucdn.cn/api/v1/station_effect'
+ROOM_API = 'http://open.douyucdn.cn/api/RoomApi/room/%s'
 
 
 class RoomInfo(threading.Thread):
-    def __init__(self, room_id, win, *args):
+    status = {
+        'now_time': None,
+        'room_id': None,
+        'room_name': None,
+        'owner_name': None,
+        'avatar': None,
+        'owner_weight': None,
+        'room_thumb': None,
+        'room_status': None,
+        'start_time': None,
+        'fans_num': None,
+        'online': None,
+        'minutes': None,
+    }
+    gifts = {}
+
+    def __init__(self, room_id, gift_q, info_q, root):
         super(RoomInfo, self).__init__()
-        self.status = {
-            'room_id': None,
-            'room_name': None,
-            'owner_name': None,
-            'avatar': None,
-            'owner_weight': None,
-            'room_thumb': None,
-            'room_status': None,
-            'start_time': None,
-            'fans_num': None,
-            'online': None,
-        }
         self.room_id = room_id
-        self.args = args
-        self.win = win
-        self.stop = False
-        self.live = False
+        self.room_api = ROOM_API % self.room_id
+        self.room_thumb = os.path.join(os.getcwd(), 'room_%s.jpg' % self.room_id)
+        self.gift_q = gift_q
+        self.info_q = info_q
+        self.root = root
+        self.alive = threading.Event()
+        self.alive.set()
 
     def run(self):
-        room_api = 'http://open.douyucdn.cn/api/RoomApi/room/%s' % self.room_id
-        while True:
-            if self.stop:
-                raise SystemExit
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.init(loop))
+        loop.run_forever()
+        loop.close()
 
-            try:
-                with urllib.request.urlopen(room_api) as f:
-                    html = f.read().decode('utf-8')
-            except (IncompleteRead, URLError, ConnectionRefusedError, ConnectionResetError):
-                time.sleep(1)
-                continue
-            except Exception as e:
-                logger.exception(e)
-                time.sleep(1)
-                continue
+    async def init(self, loop):
+        while self.alive:
+            async with aiohttp.ClientSession() as session:
+                room_info = await self.update_info(session)
+                gifts_info = await self.update_gift(session)
+                now = time.localtime()
 
-            room_info = json.loads(html)['data']
-            gift_info = room_info['gift']
-            for k in self.status:
-                self.status[k] = room_info[k]
-            for gift in gift_info:
-                utils.gifts[gift['id']] = gift['name']
+                for key in self.status:
+                    if key == 'now_time':
+                        self.status[key] = time.strftime('%m-%d %H:%M:%S', now)
+                    elif key == 'room_thumb':
+                        self.status[key] = await self.update_pic(session, room_info['room_thumb'])
+                    elif key == 'room_name':
+                        self.status[key] = unescape(room_info[key])
+                    elif key == 'minutes' and room_info['room_status'] == '1':
+                        start = room_info['start_time']
+                        start = time.strptime(start, '%Y-%m-%d %H:%M')
+                        minutes = int((time.mktime(now) - time.mktime(start)) / 60)
+                        self.status[key] = minutes
+                    elif key == 'minutes':
+                        pass
+                    else:
+                        self.status[key] = room_info[key]
 
-            try:
-                with urllib.request.urlopen(gift_api) as f:
-                    html = f.read().decode('utf-8')
-            except (IncompleteRead, URLError, ConnectionRefusedError, ConnectionResetError):
-                time.sleep(1)
-                continue
-            except Exception as e1:
-                logger.exception(e1)
-                time.sleep(1)
-                continue
+                gifts_info.extend(room_info['gift'])
+                for gift in gifts_info:
+                    self.gifts[gift['id']] = gift['name']
 
-            gifts_info = json.loads(html)['data']['prop_gift']
-            for gift in gifts_info:
-                utils.gifts[gift['id']] = gift['name']
-
-            now = time.localtime()
-            now_str = time.strftime('%Y-%m-%d %H:%M:%S', now)
-
-            if self.status['room_status'] == '2':
-                self.status['room_status'] = '下播了'
-                if self.live:
-                    self.live = False
-                self.update_info(now_str)
-            elif self.status['room_status'] == '1':
-                start = self.status['start_time']
-                start = time.strptime(start, '%Y-%m-%d %H:%M')
-                minute = int((time.mktime(now) - time.mktime(start)) / 60)
-                self.status['room_status'] = '直播中(已播%s分钟)' % minute
-                self.update_info(now_str)
-                if not self.live:
-                    self.popup(self.status['room_name'], minute, self.status['owner_name'],
-                               self.status['room_thumb'], self.room_id)
-                    self.live = True
-
+            self.info_q.put(self.status)
+            self.root.event_generate('<<ROOMINFO>>')
+            self.gift_q.put(self.gifts)
             time.sleep(10)
 
-    def update_info(self, now):
-        str_1, str_2, str_3, str_4, str_5, str_6, str_7, str_8 = self.args
-        str_1.set(unescape(self.status['room_name']))
-        str_2.set(self.status['owner_name'])
-        str_3.set(self.status['room_status'])
-        str_4.set(self.status['fans_num'])
-        str_5.set(self.status['owner_weight'])
-        str_6.set(self.status['online'])
-        str_7.set(self.status['start_time'])
-        str_8.set(now)
+        loop.stop()
 
-    def popup(self, title, live, name, image, room_id):
-        self.win.name_text.set(name)
-        self.win.status_text.set('直播中(已播%s分钟)' % live)
-        self.win.title_text.set(title)
-        self.win.image = image
-        self.win.room_id = room_id
-        self.win.pop_up()
+    async def update_info(self, session):
+        res = await fetch_json(session, self.room_api)
+        room_info = res['data']
+        return room_info
+
+    async def update_gift(self, session):
+        res = await fetch_json(session, GIFT_API)
+        gift_info = res['data']['prop_gift']
+        return gift_info
+
+    async def update_pic(self, session, url):
+        if url == self.status['room_thumb']:
+            pass
+        else:
+            res = await fetch_read(session, url)
+            with open(self.room_thumb, 'wb') as f:
+                f.write(res)
+        return url
+
+    def quit(self):
+        self.alive.clear()
+
+
+async def fetch_json(session, url):
+    async with session.get(url) as res:
+        return await res.json()
+
+async def fetch_read(session, url):
+    async with session.get(url) as res:
+        return await res.read()
