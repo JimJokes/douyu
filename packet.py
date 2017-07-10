29167953bda2434db8945c196bdaef2ef1d34b83
JimJokes <543001037@qq.com>
2017-07-10 17:08:13 +0800

    remove useless code

--- packet.py
+++ packet.py
@@ -20,38 +20,6 @@ class Packet:
         return pack('<llhbb%ds' % (len(self.body) + 1), raw_length, raw_length,
                     msg_type, 0, 0, (self.body + '\0').encode())
 
-    def size(self):
-        if self.body is None:
-            return 0
-        return len(self.body) + 12
-
-    @staticmethod
-    def sniff(buff):
-
-        buff_len = len(buff)
-
-        if buff_len < 12:
-            return None
-
-        packet_length_1, packet_length_2, msg_type, encryption, reserved, body = unpack('<llhbb%ds' % (buff_len - 12), buff)
-
-        if packet_length_1 != packet_length_2:
-            # print(packet_length_1 + packet_length_2)
-            # print(body)
-            logger.warning('[Packet] Unmatched packet length fields!')
-            return None
-
-        needed_body_length = packet_length_1 - 8
-        current_body_length = len(body)
-
-        if current_body_length < needed_body_length:
-            return None
-
-        if current_body_length > needed_body_length:
-            body = body[0:needed_body_length]
-
-        return Packet(body)
-
     @staticmethod
     def header_sniff(buff):
         packet_length_1, packet_length_2, msg_type, encryption, reserved = unpack('<llhbb', buff)
@@ -59,4 +27,4 @@ class Packet:
         if packet_length_1 != packet_length_2:
             raise UnmatchedLengthError('[Packet] Unmatched packet length fields!')
 
-        return packet_length_1-12
+        return packet_length_1-8
