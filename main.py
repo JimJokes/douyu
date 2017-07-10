63844c5ec22843692db42d4b83a34494b35323dd
JimJokes <543001037@qq.com>
2017-07-10 17:07:30 +0800

    rewrite for new version

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from window import View
+import tkinter as tk
 import encodings.idna
+
+from ttkWindow import App
 from logger import Logger
 
-__version__ = 'v2.0'
+__version__ = 'v2.0beta'
 
 if __name__ == '__main__':
     logger = Logger(name='main')
-    root = View(1000, 600)
+    root = tk.Tk()
+    App(root)
     root.title('斗鱼弹幕姬%s   by：缘总包养的小三儿' % __version__)
     root.minsize(height=500, width=800)
     root.mainloop()
