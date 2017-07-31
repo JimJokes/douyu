0a5e23e322614ef309a33d0f57e12a2a418b88fd
JimJokes <543001037@qq.com>
2017-07-31 17:29:48 +0800

    版本更新到v2.1.0

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import encodings.idna
 from ttkWindow import App
 from logger import Logger
 
-__version__ = 'v2.0.1'
+__version__ = 'v2.1.0'
 
 if __name__ == '__main__':
     logger = Logger(name='main')
     root = tk.Tk()
     App(root)
     root.title('斗鱼弹幕姬%s   by：缘总包养的小三儿' % __version__)
-    root.minsize(height=500, width=800)
+    # root.minsize(height=500, width=800)
     root.mainloop()
